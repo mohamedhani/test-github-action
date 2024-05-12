f729304fc994b5d7435c32f3226ed73e6e75567a
mohamedhani <mohamedhani01014@gmail.com>
2024-05-12 17:47:24 +0300

    add helm chart and refactor workflows (#10)
    * add helm chart and refactor workflows
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * add healthcheck
    
    * add healthcheck
    
    * fix issues in ingress
    
    * add delete protection to ingress
    
    * fix issue in dockerization workflow
    
    * fix issue in helm chart
    
    * fix issue
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * add uninstall stage release on pull closed
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing

--- router/setup.go
+++ router/setup.go
@@ -7,9 +7,12 @@ import (
 func ping_route(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{"message": "pong"})
 }
-
+func health_check(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{"message": "healthy"})
+}
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", ping_route)
+	r.GET("/healthz", health_check)
 	return r
 }
